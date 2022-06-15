d98d591f0584b5631ef9f0f8c62d71f281671f93
Ammar Husain <ammar.s.husain@gmail.com>
2022-06-15 07:55:47 -0400

    Polydisk + reference
    Polydisk capacities and comments

--- capacities.hs
+++ capacities.hs
@@ -1,8 +1,13 @@
 import Data.List
 import Data.Set
 
+-- See https://math.berkeley.edu/~hutching/beyond.pdf for these formulas and sources for their proofs
+
 -- the k'th ECH capacity of ellipsoids with integer symplectic radii a and b
 ellipsoid_capacity a b k = (sort $ toList $ fromList $ (<*> id) (\xs ys -> [a*x+b*y| x<-xs,y<-ys]) [0..k]) !! k
+
+-- the k'th ECH capacity of polydisks, a/b do not have to be integer in this case
+polydisk_cap a b k = minimum $ (<*> id) (\xs ys -> [a*x+b*y | x <- xs, y<-ys, (x+1)*(y+1)>k+1]) [0..k]
 
 -- cap function
 cap_function a b r = Data.List.filter (<=r) $ fmap (ellipsoid_capacity a b) [0..r]
